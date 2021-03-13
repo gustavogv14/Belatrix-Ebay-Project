f2a7ce83b62403c2070e572b2c0f890b8a37792c
gustavouniandes <gustavogv14@hotmail.com>
2021-03-13 16:25:52 -0500

    Hola Mundo

---
+++ tests/test_album.py
@@ -0,0 +1,10 @@
+import unittest
+
+from src.logica.coleccion import Coleccion
+from src.modelo.album import Album, Medio
+from src.modelo.declarative_base import Session
+
+class AlbumTestCase(unittest.TestCase):
+
+    def test_prueba(self):
+        self.assertEqual(0, 0)
